1272fd06a7766648c689f2ebc6aea99c6b362617
veerbhadrasolanki <veerbhadrasingh123@gmail.com>
2022-05-03 22:30:57 +0530

    Fourth commit

---
+++ String/rotationOfString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class rotationOfString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S1 = sc.nextLine();
+        String S2 = sc.nextLine();
+        String S3 = S1 + S1;
+        for (int i = 0; i < S3.length(); i++) {
+            for (int j = 0; j < S2.length(); j++) {
+
+            }
+        }
+    }
+}
+
+// System.out.println("Yes");
+// if (S3.charAt(i) == S2.charAt(j)) {
+// if (j++ >= S2.length()) {
+// System.out.println("Pass");
+// } else {
+// continue;
+// }
+// } else {
+// j = 0;
+// break;
+// }
