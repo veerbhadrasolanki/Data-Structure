80f1d83efd1190a2a8949e762f81f528abe18fe5
veerbhadrasolanki <veerbhadrasingh123@gmail.com>
2022-05-03 22:31:14 +0530

    fourth commit

--- String/rotationOfString.java
+++ String/rotationOfString.java
@@ -6,22 +6,7 @@ public class rotationOfString {
         String S1 = sc.nextLine();
         String S2 = sc.nextLine();
         String S3 = S1 + S1;
-        for (int i = 0; i < S3.length(); i++) {
-            for (int j = 0; j < S2.length(); j++) {
+        System.out.println(S3.contains(S2));
 
-            }
-        }
     }
 }
-
-// System.out.println("Yes");
-// if (S3.charAt(i) == S2.charAt(j)) {
-// if (j++ >= S2.length()) {
-// System.out.println("Pass");
-// } else {
-// continue;
-// }
-// } else {
-// j = 0;
-// break;
-// }
