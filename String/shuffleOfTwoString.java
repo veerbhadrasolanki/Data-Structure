d7914bc09bac3023f47007e68f09d5e459610594
veerbhadrasolanki <veerbhadrasingh123@gmail.com>
2022-05-04 23:58:07 +0530

    new changes

--- String/shuffleOfTwoString.java
+++ String/shuffleOfTwoString.java
@@ -8,14 +8,24 @@ public class shuffleOfTwoString {
         String S2 = sc.nextLine();
         String Check = sc.nextLine();
         String temp = S1 + S2;
-        /*
-         * char[] dump1 = temp.toCharArray();
-         * char[] dump2 = Check.toCharArray();
-         * Arrays.sort(dump1);
-         * Arrays.sort(dump2);
-         */
-        if (temp.length() == Check.length()) {
-            System.out.println(temp.contains(Check));
+
+        char[] dump1 = temp.toCharArray();
+        char[] dump2 = Check.toCharArray();
+        Arrays.sort(dump1);
+        Arrays.sort(dump2);
+        boolean flag = true;
+        if (dump1.length == dump2.length) {
+            for (int i = 0; i < dump1.length; i++) {
+                if (dump1[i] == dump2[i]) {
+                    flag = true;
+                } else {
+                    flag = false;
+                }
+            }
+        }
+
+        if (flag == true) {
+            System.out.println("Shuffled");
         } else {
             System.out.println("Not Shuffled");
         }
