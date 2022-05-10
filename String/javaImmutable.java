0a5f481194efe05b36fa771d5559554a947a35b4
veerbhadrasolanki <veerbhadrasingh123@gmail.com>
2022-05-10 22:44:28 +0530

    nw

---
+++ String/javaImmutable.java
@@ -0,0 +1,3 @@
+Why String is Immutable in Java?
+
+Because of the presence of the String pool in the preceding example, two different variables are pointing to same String object from the pool, thus saving crucial memory resource.
