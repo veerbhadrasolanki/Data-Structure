23d94b8a13613ee3886e02b41e018eb7f73607ad
veerbhadrasolanki <veerbhadrasingh123@gmail.com>
2022-05-24 22:04:44 +0530

    binarytree

--- BinaryTree/binarytree.java
+++ BinaryTree/binarytree.java
@@ -1,9 +1,20 @@
 class bt{
-
+  int node;
+  bt left;
+  bt right;
+  bt( int x ){
+    node = x;
+    left = null;
+    right = null;
+  }
 }
 
 public class binarytree {
     public static void main(String []args) {
-        
+      bt tree = new bt();
+      tree.root = new bt(20);
+      tree.root.right = new bt(30);
+      System.out.println((int)root );
+      System.out.println(root.left);
     }
 }
